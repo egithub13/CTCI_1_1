5765636274ae36b7b8b84db83d419924e954370c
ehayes <ehayes15@student.umuc.edu>
2022-09-16 16:30:43 -0400

    Initial Commit

---
+++ CTCI_1_1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CTCI_1_1
+//
+//  Created by eric hayes on 9/16/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
