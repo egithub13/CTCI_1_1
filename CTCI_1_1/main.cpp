c9dc7504e14e3edeb42265839000501a6fa0d289
ehayes <ehayes15@student.umuc.edu>
2022-09-16 18:40:01 -0400

    Completed 1.1 with bitwise operation.

--- CTCI_1_1/main.cpp
+++ CTCI_1_1/main.cpp
@@ -6,10 +6,32 @@
 //
 
 #include <iostream>
+#include <string>
+
+bool isUnique(std::string s) {
+  sort(s.begin(), s.end()); // O(n Log n) sort
+  
+  for (int i = 1; i < s.size(); ++i) {
+    if (s[i] == s[i - 1]) { return false; }
+  }
+  return true;
+}
+
+bool isUn(std::string s) {
+  int checker = 0;
+  for (int i = 0; i < s.size(); ++i) {
+    int val = s[i] - 'a';
+    if ((checker & (1 << val)) > 0) { return false; }
+    checker |= (1 << val);
+    
+  }
+  return true;
+}
 
 int main(int argc, const char * argv[]) {
   // insert code here...
-  std::cout << "Hello, World!\n";
+  std::string demo = "abcdefghijklmnop";
+  std::cout << isUn(demo) << std::endl;
   
   return 0;
 }
